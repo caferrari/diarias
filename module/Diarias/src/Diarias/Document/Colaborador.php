9b1e12d494de63ad20fcf1eed6c1f2508e2eda67
Carlos A. Ferrari <caferrari@gmail.com>
2013-03-01 10:24:03 -0300

    mais testes

--- module/Diarias/src/Diarias/Document/Colaborador.php
+++ module/Diarias/src/Diarias/Document/Colaborador.php
@@ -4,25 +4,42 @@ namespace Diarias\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 
-/** @ODM\Document */
+/** @ODM\Document(collection="colaborador") */
 class Colaborador
 {
     /** @ODM\Id */
     private $id;
 
     /** @ODM\String */
-    private $name;
+    private $nome;
 
     /** @ODM\String */
     private $email;
 
+    /** @ODM\Int */
+    private $matricula;
+
     public function __set($key, $value)
     {
-        $this->$key = $value; //$this->valid($key, $value);
+        $setMethod = "set" . ucfirst($key);
+        if (method_exists($this, $setMethod)) {
+            $this->$setMethod($value);
+            return;
+        }
+
+        if (!in_array($key, array_keys(get_object_vars($this)))) {
+            throw new \InvalidArgumentException("Propriedade $key não existe");
+        }
+
+        $this->$key = $value;
     }
 
     public function __get($key)
     {
         return $this->$key;
+    }
+
+    public function setNome($nome) {
+        $this->nome = mb_strtoupper($nome);
     }
 }
--- module/Diarias/tests/src/Diarias/Document/ColaboradorTest.php
+++ module/Diarias/tests/src/Diarias/Document/ColaboradorTest.php
@@ -7,24 +7,75 @@ use Test\TestCase;
 class ColaboradorTest extends TestCase
 {
 
-    public function test_se_insere_no_mongo()
+    public function testSeClasseColaboradorExiste()
+    {
+        $this->assertTrue(class_exists('Diarias\Document\Colaborador'));
+        $c = new Document\Colaborador;
+        $this->assertTrue(get_class($c) == 'Diarias\Document\Colaborador');
+    }
+
+    /**
+      * @dataProvider providerCampos
+      */
+    public function testCampos($campo, $valor) {
+        $c = new Document\Colaborador;
+
+        $campos = get_object_vars($c);
+
+        try {
+            $c->$campo = $valor;
+            $this->assertEquals($valor, $c->$campo);
+        } catch (\InvalidArgumentException $e) {
+            $this->fail("Propriedade \"$campo\" não existe");
+        }
+    }
+
+    /**
+      * @dataProvider providerNomes
+      */
+    public function testSeConverteNomesParaMaiusculas($nomeOriginal, $nomeEsperado) {
+        $c = new Document\Colaborador;
+        $c->nome = $nomeOriginal;
+        $this->assertEquals($nomeEsperado, $c->nome);
+    }
+
+    public function testSeInsereColaboradorNoMongo()
     {
         $dm = $this->getDm();
 
+        $collection = $dm->getDocumentCollection('Diarias\Document\Colaborador');
+        $collection->drop();
+
         $c = new Document\Colaborador;
 
-        $c->name = 'Carlos André Ferrari';
+        $c->nome = 'Carlos André Ferrari';
         $c->email = 'caferrari@gmail.com';
 
         $dm->persist($c);
         $dm->flush();
 
+        $this->assertEquals(1, $collection->count());
+
         $r = $dm->getRepository('Diarias\Document\Colaborador');
-
         $obj = $r->findOneById($c->id);
-
         $this->assertEquals($obj, $c);
-
     }
+
+    public function providerNomes() {
+        return array(
+            array('carlos andré ferrari', 'CARLOS ANDRÉ FERRARI'),
+            array('carlos AndRé Ferrari', 'CARLOS ANDRÉ FERRARI'),
+            array('áéíóú bla', 'ÁÉÍÓÚ BLA')
+        );
+    }
+
+    public function providerCampos() {
+        return array(
+            array('id', 1),
+            array('nome', 'FULANO'),
+            array('matricula', 123)
+        );
+    }
+
 
 }
