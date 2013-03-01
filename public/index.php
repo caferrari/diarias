e15fe8c8727accad219dd9c612b00e4761450221
Carlos A. Ferrari <caferrari@gmail.com>
2013-03-01 10:23:33 -0300

    UTF-8 como charset padrão

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
+
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+mb_http_input('UTF-8');
+mb_language('uni');
+mb_regex_encoding('UTF-8');
+ob_start('mb_output_handler');
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
