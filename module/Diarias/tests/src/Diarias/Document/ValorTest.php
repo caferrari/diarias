6491d8b6b59b34331d04f9026eafd03c85dae8f9
Carlos A. Ferrari <caferrari@gmail.com>
2013-03-04 09:30:39 -0300

    Added entity and tests for the Valor collection

---
+++ module/Diarias/tests/src/Diarias/Document/ValorTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Diarias;
+
+use Test\TestCase;
+
+class ValorTest extends TestCase
+{
+
+    public function testSeClasseOrgaoExiste()
+    {
+        $this->assertTrue(class_exists('Diarias\Document\Valor'));
+    }
+
+    /**
+      * @dataProvider providerCampos
+      */
+    public function testCampos($campo, $valor) {
+        $c = new Document\Valor;
+        $campos = get_object_vars($c);
+
+        try {
+            $c->$campo = $valor;
+            $this->assertEquals($valor, $c->$campo);
+        } catch (\InvalidArgumentException $e) {
+            $this->fail("Propriedade \"$campo\" não existe");
+        }
+    }
+
+    public function testSeInsereValorNoMongo()
+    {
+        $dm = $this->getDm();
+
+        $collection = $dm->getDocumentCollection('Diarias\Document\Valor');
+
+        $c = new Document\Valor;
+        $c->valor_capital_estado = 110.5;
+        $c->valor_capital = 110.60;
+        $c->valor_interior = 110.55;
+        $c->cargos = array('DAS-5', 'DAS-10', 'CPC IV');
+        $dm->persist($c);
+        $dm->flush();
+
+        $this->assertEquals(1, $collection->count());
+
+        $c = new Document\Valor;
+        $c->valor_capital_estado = 110.5;
+        $c->valor_capital = 110.60;
+        $c->valor_interior = 110.55;
+        $c->cargos = array('DAS-6', 'DAS-9', 'CPC III', 'Diretor de Mídias integradas');
+        $dm->persist($c);
+        $dm->flush();
+
+        $this->assertEquals(2, $collection->count());
+
+        $r = $dm->getRepository('Diarias\Document\Valor');
+        $obj = $r->findOneById($c->id);
+        $this->assertEquals($obj, $c);
+    }
+
+    public function testSeRetornaExceptionAoRepetirCargos() {
+
+    }
+
+    public function providerCampos() {
+        return array(
+            array('id', 1),
+            array('valor_capital_estado', 125.50),
+            array('valor_capital', 125.50),
+            array('valor_interior', 125.50),
+        );
+    }
+
+
+}
